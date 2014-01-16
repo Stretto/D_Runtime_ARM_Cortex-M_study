3b988da1eceb1e3f81f39fa84cc75026b7a59eec
Mike <slavo5150@yahoo.com>
2014-01-16 22:18:10 +0900

    initial commit

--- source/start.d
+++ source/start.d
@@ -1,104 +1,54 @@
-//           Copyright Michael V. Franklin 2014
-// Distributed under the Boost Software License, Version 1.0.
-//    (See copy at http://www.boost.org/LICENSE_1_0.txt)
+// start.d
 
 module start;
 
-import trace;
-import memory;
-import reference;
-import isr;
-  
-extern(C) __gshared void * _Dmodule_ref;
+alias extern(C) void function() ISR;
+extern(C) immutable ISR ResetHandler = &OnReset;
 
-extern(C) void _d_assert_msg(string msg, string file, uint line)
+void SendCommand(int command, void* message)
 {
-    Trace.WriteLine(file, ":", line, ": ", msg);
+  // LDC and GDC use slightly different inline assembly syntax, so we have to 
+  // differentiate them with D's conditional compilation feature, version.
+  version(LDC)
+  {
+    __asm
+    (
+      "mov r0, $0;
+      mov r1, $1;
+      bkpt #0xAB",
+      "r,r,~{r0},~{r1}",
+      command, message
+    );
+  }
+  else version(GNU)
+  {
+    asm
+    {
+      "mov r0, %[cmd]; 
+       mov r1, %[msg]; 
+       bkpt #0xAB"
+	:                              
+	: [cmd] "r" command, [msg] "r" message
+	: "r0", "r1";
+    };
+  }
 }
 
-extern(C) void _d_assert(string file, uint line)
+// The program's entry point
+extern(C) void OnReset()
 {
-    Trace.WriteLine(file, ":", line);
-}
-
-extern(C) void _Unwind_Resume(void *ucbp)
-{
-    Trace.WriteLine("_Unwind_Resume");
-}
+  // run repeatedly
+  while(true)
+  {
+    // Create semihosting message
+    uint[3] message =
+      [
+	2, 			            // stderr
+	cast(uint)"Hello, World!\r\n".ptr,  // ptr to string
+	15                                  // number of bytes in string
+      ];
  
-__gshared int GlobalDataVar = 2;
-__gshared int GlobalBssVar;
-
-
-extern (C) Object _d_newclass(const ClassInfo ci)
-{
-    void* p;
-
-    p = HeapMemory.Instance.Allocate(ci.init.length);
-    
-    return cast(Object) p;
-}
-
-extern(C) extern __gshared uint __data_rom_begin;
-extern(C) extern __gshared uint __data_ram_begin;
-extern(C) extern __gshared uint __data_ram_end;
-extern(C) extern __gshared uint __bss_begin;
-extern(C) extern __gshared uint __bss_end;
-
-class MyClass
-{
-    uint x;
-    
-    this()
-    {
-	Trace.WriteLine("Constructor");
-    }
-    
-    ~this()
-    {
-	Trace.WriteLine("Destructor");
-    }
-}
-
-void DoFunction()
-{     
-    auto r1 = Reference!MyClass.Create();
-    auto r2 = Reference!MyClass.Create();
-    
-    r1.x = 11;
-    r2.x = 22;
-    
-    Trace.WriteLine("r1: ", r1.Count, ": ", r1.x);
-    Trace.WriteLine("r2: ", r2.Count, ": ", r2.x);
-    
-    r1 = r2;	
-    
-    Trace.WriteLine("r1: ", r1.Count, ": ", r1.x);
-    Trace.WriteLine("r2: ", r2.Count, ": ", r2.x);
-    
-    MyClass c = new MyClass();
-    c.x = 10;
-    
-    c.destroy();
-}
-
-extern(C) private void OnHardFault()
-{
-    Trace.WriteLine("Hard Fault");
-    while(true)
-    { }
-}
-
-extern(C) private void OnReset()
-{
-    Trace.Write("Copying...");
-    memcpy(&__data_ram_begin, &__data_rom_begin, cast(size_t)(&__data_ram_end) - cast(size_t)(&__data_ram_begin) + 1);
-    memset(&__bss_begin, 0, cast(size_t)(&__bss_end) - cast(size_t)(&__bss_begin) + 1);
-    Trace.WriteLine("Done");
-    
-    DoFunction();
-    
-    Trace.WriteLine("Done"); 
-    while(true)
-    { }
+    // Send semihosting command
+    SendCommand(0x05, &message);
+  }
 }
