e50acd696a8fce648e0eddc9b5cafa2329077bb6
Mike <slavo5150@yahoo.com>
2014-01-25 14:35:50 +0900

    Added _d_assert_msg

--- source/assert.d
+++ source/assert.d
@@ -2,7 +2,18 @@
 
 import trace;
 
+/***********************************************************************
+  For assert(condition) statements
+*/
 extern(C) void _d_assert(string file, uint line)
 {
     trace.WriteLine(file, line);
+}
+
+/***********************************************************************
+  For assert(condition, message) statements
+*/
+extern(C) void _d_assert_msg(string msg, string file, uint line)
+{
+    trace.WriteLine(file, line, msg);
 }
