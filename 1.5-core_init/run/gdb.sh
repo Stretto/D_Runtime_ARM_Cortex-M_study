99ca6a6575928f12ebba28e94ec96412d945146d
Mike <slavo5150@yahoo.com>
2014-02-22 00:38:35 +0900

    Removed intermediate directories

--- 1.5-core_init/run/gdb.sh
+++
@@ -1,2 +0,0 @@
-~/gcc-arm-none-eabi/bin/arm-none-eabi-gdb ../binary/start.elf -ex "target remote localhost:3333" \
-    -ex "monitor arm semihosting enable" 
--- 1.5-core_init/run/openocd.sh
+++
@@ -1,1 +0,0 @@
-openocd -f interface/jtag-lock-pick_tiny_2.cfg -f target/stm32f4x.cfg
